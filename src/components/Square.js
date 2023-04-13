5acf5087f832c7dc9100794931cfad7813d4d2d3
AtulRawat001010 <workerhere12@gmail.com>
2023-04-14 01:50:13 +0530

    #

--- src/components/Square.js
+++ src/components/Square.js
@@ -3,7 +3,7 @@ export default function Square({value, onSquareClick}) {
 
   return (
     <div>
-        <div className="btn-square" onClick={onSquareClick}>{value}</div>
+        <div className="square-btn" onClick={onSquareClick}>{value}</div>
     </div>
   )
 }
